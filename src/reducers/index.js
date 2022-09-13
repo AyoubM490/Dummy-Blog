53eb113cef991257178fd339fa9e2f76a5af31c8
AyoubMS <chahayb563@gmail.com>
2022-09-13 13:31:21 +0100

    Posts reducer with tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import successReducer from './successReducer';
+import posts from './posts/reducer';
 
 export default combineReducers({
-  success: successReducer,
+  posts,
 });
