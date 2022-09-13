f75f9bc6d6e2bf818f54ad8362e393140a108d50
AyoubMS <chahayb563@gmail.com>
2022-09-13 22:39:22 +0100

    add App test

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     };
 
     return (
-      <div className="App">
+      <div className="App" data-test="appComponent">
         <Header />
         <section className="main">
           <Headline
