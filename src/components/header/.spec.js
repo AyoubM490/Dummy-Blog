ecbfa1b6b548aac76e7e9814f81545e7420fcada
AyoubMS <chahayb563@gmail.com>
2022-09-13 17:19:59 +0100

    correct failing tests

--- src/components/header/.spec.js
+++ src/components/header/.spec.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Header from "./index";
-import findByTestAttr from "../../../Utils";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './index';
+import findByTestAttr from '../../Utils';
 
 const setUp = (props = {}) => {
   const component = shallow(<Header {...props} />);
   return component;
 };
 
-describe("Header Component", () => {
+describe('Header Component', () => {
   let component;
   beforeEach(() => {
     component = setUp();
   });
 
-  it("Should render without errors", () => {
-    const wrapper = findByTestAttr(component, "headerComponent");
+  it('Should render without errors', () => {
+    const wrapper = findByTestAttr(component, 'headerComponent');
     expect(wrapper.length).toBe(1);
   });
 
-  it("Should render a logo", () => {
-    const logo = findByTestAttr(component, "logoImg");
+  it('Should render a logo', () => {
+    const logo = findByTestAttr(component, 'logoImg');
     expect(logo.length).toBe(1);
   });
 });
