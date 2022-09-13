7da4818fe32412d232ef8b7a54f73239caab4a6b
AyoubMS <chahayb563@gmail.com>
2022-09-13 16:24:17 +0100

    update tests

--- src/components/headline/.spec.js
+++ src/components/headline/.spec.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Headline from "./index";
-import findByTestAttr from "../../../Utils/index";
-import { checkProps } from "../../../Utils/index";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Headline from './index';
+import findByTestAttr from '../../Utils';
+import { checkProps } from '../../Utils/index';
 
 const setUp = (props = {}) => {
   const component = shallow(<Headline {...props} />);
   return component;
 };
 
-describe("Headline Component", () => {
-  describe("Checking PropTypes", () => {
-    it("Should not throw a warning", () => {
+describe('Headline Component', () => {
+  describe('Checking PropTypes', () => {
+    it('Should not throw a warning', () => {
       const expectedProps = {
-        header: "Test Header",
-        desc: "Test Desc",
+        header: 'Test Header',
+        desc: 'Test Desc',
         tempArr: [
           {
-            fName: "Test fName",
-            lName: "Test lName",
-            email: "test@email.com",
+            fName: 'Test fName',
+            lName: 'Test lName',
+            email: 'test@email.com',
             age: 23,
             onlineStatus: false,
           },
@@ -31,40 +31,40 @@ describe("Headline Component", () => {
     });
   });
 
-  describe("Have props", () => {
+  describe('Have props', () => {
     let wrapper;
     beforeEach(() => {
       const props = {
-        header: "Test Header",
-        desc: "Test Desc",
+        header: 'Test Header',
+        desc: 'Test Desc',
       };
       wrapper = setUp(props);
     });
 
-    it("Should render without errors", () => {
-      const component = findByTestAttr(wrapper, "HeadlineComponent");
+    it('Should render without errors', () => {
+      const component = findByTestAttr(wrapper, 'HeadlineComponent');
       expect(component.length).toBe(1);
     });
 
-    it("Should render a H1", () => {
-      const h1 = findByTestAttr(wrapper, "header");
+    it('Should render a H1', () => {
+      const h1 = findByTestAttr(wrapper, 'header');
       expect(h1.length).toBe(1);
     });
 
-    it("Should render a desc", () => {
-      const desc = findByTestAttr(wrapper, "desc");
+    it('Should render a desc', () => {
+      const desc = findByTestAttr(wrapper, 'desc');
       expect(desc.length).toBe(1);
     });
   });
 
-  describe("Have NO props", () => {
+  describe('Have NO props', () => {
     let wrapper;
     beforeEach(() => {
       wrapper = setUp();
     });
 
-    it("Should not render", () => {
-      const component = findByTestAttr(wrapper, "headlineComponent");
+    it('Should not render', () => {
+      const component = findByTestAttr(wrapper, 'headlineComponent');
       expect(component.length).toBe(0);
     });
   });
